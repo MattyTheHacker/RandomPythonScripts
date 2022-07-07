386cbc8166b53aa6b8baf2af556b2de56576a1f9
Matty <matty258987@gmail.com>
2022-07-07 19:34:09 +0100

    more Qs

--- interviewquestions.py
+++ interviewquestions.py
@@ -27,6 +27,77 @@ def check_if_two_strings_are_anagrams(string_one, string_two):
 
 
 
+def remove_duplicates_from_list(input_list):
+    return list(set(input_list))
+
+
+def find_all_pairs_that_sum_to_x(list, x):
+    pairs = []
+    for i in range(len(list)):
+        for j in range(i+1, len(list)):
+            if list[i] + list[j] == x:
+                pairs.append((list[i], list[j]))
+    return pairs
+
+
+def find_unique_pairs_that_sum_to_x(list, x):
+    pairs = []
+    for i in range(len(list)):
+        for j in range(i+1, len(list)):
+            if list[i] + list[j] == x:
+                pairs.append((list[i], list[j]))
+    return set(pairs)
+
+
+def is_palindrome(string):
+    if string == string[::-1]:
+        return True
+    return False
+
+
+def intersection_of_lists(list_one, list_two):
+    return list(set(list_one) & set(list_two))
+
+
+def union_of_lists(list_one, list_two):
+    return list(set(list_one) | set(list_two))
+
+
+def difference_of_lists(list_one, list_two):
+    return list(set(list_one) - set(list_two))
+
+
+def check_if_list_is_subset_of_another_list(list_one, list_two):
+    if set(list_one).issubset(set(list_two)):
+        return True
+    return False
+
+
+def reverse_string(string):
+    return string[::-1]
+
+
+
+def reverse_string_recursive(string):
+    if len(string) == 1:
+        return string
+    return string[-1] + reverse_string_recursive(string[:-1])
+
+
+def reverse_string_iterative(string):
+    reversed_string = ""
+    for i in range(len(string)):
+        reversed_string += string[-i-1]
+    return reversed_string
+
+
+def find_fibonacci_number(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return find_fibonacci_number(n-1) + find_fibonacci_number(n-2)
+
 
 
 
@@ -48,6 +119,9 @@ print(find_first_duplicate_int_in_list(numbers))
 print(find_all_duplicates_int_in_list(numbers))
 print(check_if_list_contains_int(numbers, 3))
 print(check_if_two_strings_are_anagrams("abc", "cba"))
-
+print(remove_duplicates_from_list(numbers))
+print(find_all_pairs_that_sum_to_x(numbers, 6))
+print(find_unique_pairs_that_sum_to_x(numbers, 6))
+print(is_palindrome("racecar"))
 
 
