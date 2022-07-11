d393e6b3a1862929a06124e4fe31837ca6513874
Matty <matty258987@gmail.com>
2022-07-11 01:45:24 +0100

    add some tests

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,3 +1,34 @@
-from scripts import *
+from scripts.interviewquestions import *
+
+def test_check_if_list_contains_int():
+    numbers = [1, 2, 3, 4, 5]
+    for number in numbers:
+        assert check_if_list_contains_int(numbers, number) == True
+
+
+def test_intersection_of_lists():
+    list_one = [1, 2, 3, 4, 5]
+    list_two = [4, 5, 6, 7, 8]
+    assert intersection_of_lists(list_one, list_two) == [4, 5]
+
+def test_union_of_lists():
+    list_one = [1, 2, 3, 4, 5]
+    list_two = [4, 5, 6, 7, 8]
+    assert union_of_lists(list_one, list_two) == [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def test_check_if_two_strings_are_anagrams():
+    string_one = "bad"
+    string_two = "dab"
+    assert check_if_two_strings_are_anagrams(string_one, string_two) == True
+    string_one = "debit card"
+    string_two = "bad credit"
+    assert check_if_two_strings_are_anagrams(string_one, string_two) == True
+    string_one = "random string"
+    string_two = "another random string"
+    assert check_if_two_strings_are_anagrams(string_one, string_two) == False
+
+
+
 
 
